1bffa281f64a5023359d3153826d796c4e32bdb9
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-05-17 18:21:37 +0900

    refactor: ProductActivity DataBinding으로 변경

--- app/src/main/java/woowacourse/shopping/presentation/product/ProductActivity.kt
+++ app/src/main/java/woowacourse/shopping/presentation/product/ProductActivity.kt
@@ -2,7 +2,6 @@ package woowacourse.shopping.presentation.product
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,44 +14,30 @@ import woowacourse.shopping.presentation.productdetail.ProductDetailActivity
 
 class ProductActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProductBinding
-    private val productAdapter: ProductAdapter by lazy {
-        ProductAdapter { product -> navigateToProductDetail(product) }
-    }
-    private val viewModel: ProductViewModel by viewModels { ProductViewModel.Factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_product)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cl_product)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.ibCart.setOnClickListener { navigateToCart() }
 
-        initAdapter()
-        observeViewModel()
-        viewModel.fetchData()
+        initBinding()
     }
 
-    private fun initAdapter() {
-        binding.rvProducts.adapter = productAdapter
-    }
-
-    private fun observeViewModel() {
-        viewModel.products.observe(this) {
-            productAdapter.setData(it)
-        }
-    }
-
-    private fun navigateToProductDetail(product: Product) {
-        val intent = ProductDetailActivity.newIntent(this, product)
-        startActivity(intent)
+    private fun initBinding() {
+        binding.onClick = ::navigateToCart
+        binding.rvProducts.adapter = ProductAdapter { product -> navigateToProductDetail(product) }
     }
 
     private fun navigateToCart() {
-        val intent = CartActivity.newIntent(this)
-        startActivity(intent)
+        startActivity(CartActivity.newIntent(context = this))
+    }
+
+    private fun navigateToProductDetail(product: Product) {
+        startActivity(ProductDetailActivity.newIntent(context = this, product = product))
     }
 }
