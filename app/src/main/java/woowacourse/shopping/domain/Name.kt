f17fcde75ed4efeefd982611eac867bd0d3cb73a
Hwnag ChaeWon <192606356+wondroid-world@users.noreply.github.com>
2025-05-17 18:18:12 +0900

    fix: Name 도메인 isEmpty, isBlank -> isNotEmpty, isNotBlank로 변경

--- app/src/main/java/woowacourse/shopping/domain/Name.kt
+++ app/src/main/java/woowacourse/shopping/domain/Name.kt
@@ -5,7 +5,7 @@ value class Name(
     val value: String,
 ) {
     init {
-        require(value.isEmpty()) { "이름에 아무런 값이 들어오지 않았습니다." }
-        require(value.isBlank()) { "이름에 공백이 들어왔습니다." }
+        require(value.isNotEmpty()) { "이름에 아무런 값이 들어오지 않았습니다." }
+        require(value.isNotBlank()) { "이름에 공백이 들어왔습니다." }
     }
 }
